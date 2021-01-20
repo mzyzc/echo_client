41260a81b7e1221352a618c1d46cc86b308de867
mzyzc <mzyzc@czyz.xyz>
2021-01-20 12:30:03 +0000

    User details sent to server

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -2,11 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:echo_client/user.dart';
 
 class LoginPage extends StatelessWidget {
+  var username;
+  var password;
+  var displayName;
+
   @override
   Widget build(BuildContext context) {
     final inputEmail = Padding(
       padding: EdgeInsets.all(8.0),
       child: TextField(
+        onChanged: (text) => username = text,
         decoration: InputDecoration(
           border: OutlineInputBorder(),
           labelText: 'Username',
@@ -18,6 +23,7 @@ class LoginPage extends StatelessWidget {
       padding: EdgeInsets.all(8.0),
       child: TextField(
         obscureText: true,
+        onChanged: (text) => password = text,
         decoration: InputDecoration(
           border: OutlineInputBorder(),
           labelText: 'Password',
@@ -32,14 +38,47 @@ class LoginPage extends StatelessWidget {
 
     final buttonRegister = TextButton(
       child: Text('Register'),
-      onPressed: () => initNewUser(),
+      onPressed: () async {
+        displayName = await requestDisplayName(context);
+        registerUser(username, password, displayName);
+      }
     );
 
     return Scaffold(
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[inputEmail, inputPassword, buttonRegister, buttonLogin]
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[inputEmail, inputPassword, buttonRegister, buttonLogin]
       ),
     );
   }
+}
+
+Future<String> requestDisplayName(BuildContext context) {
+  var displayName;
+
+  final inputDisplayName = Padding(
+    padding: EdgeInsets.all(8.0),
+    child: TextField(
+      onChanged: (text) => displayName = text,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: 'Display name',
+      ),
+    ),
+  );
+
+  final buttonSubmit = TextButton(
+    child: Text('Submit'),
+    onPressed: () => Navigator.pop(context, displayName),
+  );
+
+  return showDialog (
+    context: context,
+    builder: (context) {
+      return SimpleDialog(
+        title: Text("Display Name"),
+        children: <Widget>[inputDisplayName, buttonSubmit],
+      );
+    }
+  );
 }
--- lib/user.dart
+++ lib/user.dart
@@ -1,7 +1,48 @@
+import 'dart:convert';
 import 'package:echo_client/keys.dart';
+import 'package:echo_client/server.dart';
 
-Future<void> initNewUser() async {
-  final keys = new Keyring()
-    ..genKeys();
-  await keys.export();
+class User {
+  String email;
+  String _password;
+  String displayName;
+  Keyring _keys;
+
+  User(this.email, this._password, this.displayName);
+
+  Future<void> register() async {
+    _keys = new Keyring()
+      ..genKeys();
+    await _keys.export();
+
+    final data = jsonEncode('''
+      "CREATE USER" [
+        {
+          "email": "${this.email}",
+          "password": "${this._password}",
+          "publicKey": "${this._keys.exchangePair.publicKey}",
+          "displayName": "${this.displayName}",
+        }
+      ]
+    ''');
+    Server.socket.write(data);
+  }
+
+  Future<void> login() async {
+    final data = jsonEncode('''
+      "READ USER" [
+        {
+          "email": "${this.email}",
+          "password": "${this._password}",
+        }
+      ]
+    ''');
+    Server.socket.write(data);
+  }
+}
+
+void registerUser(String username, String password, String displayName) {
+  final newUser = User(username, password, displayName)
+      ..register;
+  print("user ${newUser.displayName} registered with email ${newUser.email}");
 }
