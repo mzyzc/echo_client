83ab8b71aba2b72ccc2610d8e7be51b4e9cfd749
mzyzc <mzyzc@czyz.xyz>
2021-04-14 16:12:49 +0100

    Added temporary functions to get preset server responses

--- lib/server.dart
+++ lib/server.dart
@@ -55,6 +55,87 @@ class Server {
   Response read() {
     return _data;
   }
+
+  Response getMessagesTemp() {
+    final messageJson = json.encode({
+      "response": 1,
+      "messages": [
+        {
+          "id": 1,
+          "data": "Hello",
+          "mediaType": "text/plain",
+          "timestamp": "2014-02-15T08:57:47.812",
+          "signature": "cHJldGVuZCB0aGlzIGlzIGEgdmFsaWQgc2lnbmF0dXJl",
+        },
+        {
+          "id": 2,
+          "data": "Hello",
+          "mediaType": "text/plain",
+          "timestamp": "2014-02-15T08:57:47.812",
+          "signature": "cHJldGVuZCB0aGlzIGlzIGEgdmFsaWQgc2lnbmF0dXJl",
+        },
+        {
+          "id": 3,
+          "data": "Hello",
+          "mediaType": "text/plain",
+          "timestamp": "2014-02-15T08:57:47.812",
+          "signature": "cHJldGVuZCB0aGlzIGlzIGEgdmFsaWQgc2lnbmF0dXJl",
+        },
+      ]
+    });
+
+    return Response(utf8.encode(messageJson));
+  }
+
+  Response getConversationsTemp() {
+    final conversationJson = json.encode({
+      "response": 1,
+      "conversations": [
+        {
+          "id": 1,
+          "name": "Genesis",
+        },
+        {
+          "id": 2,
+          "name": "Second Hand",
+        },
+        {
+          "id": 3,
+          "name": "Lucky Lucky",
+        },
+      ]
+    });
+
+    return Response(utf8.encode(conversationJson));
+  }
+
+  Response getUsersTemp() {
+    final userJson = json.encode({
+      "response": 1,
+      "users": [
+        {
+          "id": 1,
+          "email": "@example.com",
+          "displayName": "Bobby Boss",
+          "publicKey": "a2V5",
+        },
+        {
+          "id": 2,
+          "email": "charles@example.com",
+          "displayName": "Charlie Chimney",
+          "publicKey": "a2V5",
+        },
+        {
+          "id": 3,
+          "email": "dobby@example.com",
+          "displayName": "dobby a",
+          "publicKey": "a2V5",
+        },
+      ]
+    });
+
+    return Response(utf8.encode(userJson));
+  }
 }
 
 class Response {
