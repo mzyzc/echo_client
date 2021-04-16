252198daee042290e264682bb12c1d4225276344
mzyzc <mzyzc@czyz.xyz>
2021-04-16 10:31:17 +0100

    Added more dummy data to make it look slightly more authentic

--- lib/dummy_server.dart
+++ lib/dummy_server.dart
@@ -8,21 +8,72 @@ class DummyServer {
       "messages": [
         {
           "id": 1,
-          "data": "Hello",
+          "data":
+              "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.",
           "mediaType": "text/plain",
           "timestamp": "2014-02-15T08:57:47.812",
           "signature": "cHJldGVuZCB0aGlzIGlzIGEgdmFsaWQgc2lnbmF0dXJl",
         },
         {
           "id": 2,
-          "data": "Hello",
+          "data":
+              "Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.",
           "mediaType": "text/plain",
           "timestamp": "2014-02-15T08:57:47.812",
           "signature": "cHJldGVuZCB0aGlzIGlzIGEgdmFsaWQgc2lnbmF0dXJl",
         },
         {
           "id": 3,
-          "data": "Hello",
+          "data":
+              "Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus.",
+          "mediaType": "text/plain",
+          "timestamp": "2014-02-15T08:57:47.812",
+          "signature": "cHJldGVuZCB0aGlzIGlzIGEgdmFsaWQgc2lnbmF0dXJl",
+        },
+        {
+          "id": 4,
+          "data":
+              "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.",
+          "mediaType": "text/plain",
+          "timestamp": "2014-02-15T08:57:47.812",
+          "signature": "cHJldGVuZCB0aGlzIGlzIGEgdmFsaWQgc2lnbmF0dXJl",
+        },
+        {
+          "id": 5,
+          "data":
+              "Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.",
+          "mediaType": "text/plain",
+          "timestamp": "2014-02-15T08:57:47.812",
+          "signature": "cHJldGVuZCB0aGlzIGlzIGEgdmFsaWQgc2lnbmF0dXJl",
+        },
+        {
+          "id": 6,
+          "data":
+              "Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus.",
+          "mediaType": "text/plain",
+          "timestamp": "2014-02-15T08:57:47.812",
+          "signature": "cHJldGVuZCB0aGlzIGlzIGEgdmFsaWQgc2lnbmF0dXJl",
+        },
+        {
+          "id": 7,
+          "data":
+              "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.",
+          "mediaType": "text/plain",
+          "timestamp": "2014-02-15T08:57:47.812",
+          "signature": "cHJldGVuZCB0aGlzIGlzIGEgdmFsaWQgc2lnbmF0dXJl",
+        },
+        {
+          "id": 8,
+          "data":
+              "Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.",
+          "mediaType": "text/plain",
+          "timestamp": "2014-02-15T08:57:47.812",
+          "signature": "cHJldGVuZCB0aGlzIGlzIGEgdmFsaWQgc2lnbmF0dXJl",
+        },
+        {
+          "id": 9,
+          "data":
+              "Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus.",
           "mediaType": "text/plain",
           "timestamp": "2014-02-15T08:57:47.812",
           "signature": "cHJldGVuZCB0aGlzIGlzIGEgdmFsaWQgc2lnbmF0dXJl",
@@ -39,15 +90,63 @@ class DummyServer {
       "conversations": [
         {
           "id": 1,
-          "name": "Genesis",
+          "name": "Lucas Metallo",
         },
         {
           "id": 2,
-          "name": "Second Hand",
+          "name": "Cornelia Niss",
         },
         {
           "id": 3,
-          "name": "Lucky Lucky",
+          "name": "Melanie Stockman",
+        },
+        {
+          "id": 4,
+          "name": "Tabetha Kottsick",
+        },
+        {
+          "id": 5,
+          "name": "Lashay Meggison",
+        },
+        {
+          "id": 6,
+          "name": "Mack Corporon",
+        },
+        {
+          "id": 7,
+          "name": "Jefferson Czachorowski",
+        },
+        {
+          "id": 8,
+          "name": "Louie Simms",
+        },
+        {
+          "id": 9,
+          "name": "Tinisha Bartow",
+        },
+        {
+          "id": 10,
+          "name": "Nidia Mcmilliam",
+        },
+        {
+          "id": 11,
+          "name": "Birgit Sando",
+        },
+        {
+          "id": 12,
+          "name": "Raeann Diserens",
+        },
+        {
+          "id": 13,
+          "name": "Talitha Stsauveur",
+        },
+        {
+          "id": 14,
+          "name": "Wilbert Salman",
+        },
+        {
+          "id": 15,
+          "name": "Tyler Seher",
         },
       ]
     });
@@ -61,20 +160,20 @@ class DummyServer {
       "users": [
         {
           "id": 1,
-          "email": "@example.com",
-          "displayName": "Bobby Boss",
+          "email": "wilbert@example.com",
+          "displayName": "Wilbert Salman",
           "publicKey": "a2V5",
         },
         {
           "id": 2,
-          "email": "charles@example.com",
-          "displayName": "Charlie Chimney",
+          "email": "tyler@example.com",
+          "displayName": "Tyler Seher",
           "publicKey": "a2V5",
         },
         {
           "id": 3,
-          "email": "dobby@example.com",
-          "displayName": "dobby a",
+          "email": "simms@example.com",
+          "displayName": "Louie Simms",
           "publicKey": "a2V5",
         },
       ]
